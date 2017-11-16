86ad4ad4661cfbc5c3abaf63aeacedb9d46acf41
hmainismael <hmain.ismael@hotmail.fr>
2017-11-16 14:53:35 +0100

    Task 5 (forgot to add file Utils)

---
+++ src/org/ulco/Utils.java
@@ -0,0 +1,24 @@
+package org.ulco;
+
+public class Utils {
+
+    public static GraphicsObjects selectLayer(Layer layer, Point pt, double distance) {
+        GraphicsObjects list = new GraphicsObjects();
+
+        for (GraphicsObject object : layer.getListe()) {
+            if (object.isClosed(pt, distance)) {
+                list.add(object);
+            }
+        }
+        return list;
+    }
+
+    public static GraphicsObjects select(Document document, Point pt, double distance) {
+        GraphicsObjects list = new GraphicsObjects();
+
+        for (Layer layer : document.getLayers()) {
+            list.addAll(new Utils().selectLayer(layer, pt, distance));
+        }
+        return list;
+    }
+}
