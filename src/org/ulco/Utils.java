67225e346bba11029e77faf3eef63fe311f68278
hmainismael <hmain.ismael@hotmail.fr>
2017-11-26 18:45:35 +0100

    Task 7

--- src/org/ulco/Utils.java
+++ src/org/ulco/Utils.java
@@ -1,5 +1,7 @@
 package org.ulco;
 
+import java.util.Vector;
+
 public class Utils {
 
     public static GraphicsObjects selectLayer(Layer layer, Point pt, double distance) {
@@ -19,5 +21,29 @@ public class Utils {
             list.addAll(Utils.selectLayer(layer, pt, distance));
         }
         return list;
+    }
+
+    public static Document createGridWithSquares(Point origin, int line, int column, double length) {
+        Document document  = new Document();
+        Layer layer = document.createLayer();
+
+        for (int indexX = 0; indexX < column; ++indexX) {
+            for (int indexY = 0; indexY < line; ++indexY) {
+                layer.add(new Square(new Point(origin.getX() + indexX * length, origin.getY() + indexY * length), length));
+            }
+        }
+
+        return document;
+    }
+
+    public static Document createGridWithConcentricCercles(Point center, int number, double radius, double delta) {
+        Document document  = new Document();
+        Layer layer = document.createLayer();
+
+        for (int index = 0; index < number; ++index) {
+            layer.add(new Circle(center, radius + index * delta));
+        }
+
+        return document;
     }
 }
